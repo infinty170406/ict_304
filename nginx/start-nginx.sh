198de0fe1a88ecb47af44fec77972896800c04ae
skyfall <skyfall@localhost.localdomain>
2026-06-19 17:56:58 +0100

    Revert to simple Nginx config with hardcoded upstreams

--- nginx/start-nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Remplacer les variables d'environnement dans le fichier de configuration Nginx
-envsubst '${ACCOUNT_SERVICE_URL} ${TRANSACTION_SERVICE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Démarrer Nginx
-exec nginx -g 'daemon off;'
