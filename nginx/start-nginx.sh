23c1aea66b72868c3f3d57681d6150d2ed672923
skyfall <skyfall@localhost.localdomain>
2026-06-24 01:22:50 +0100

    fix: remove nginx variables to fix dns resolution

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# Extraire l'IP du résolveur DNS du système (fourni par Render)
-RESOLVER=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
-
-# S'il n'y a pas de résolveur trouvé, utiliser Google DNS par défaut
-if [ -z "$RESOLVER" ] || [ "$RESOLVER" = " " ]; then
-    RESOLVER="8.8.8.8"
+# Remplacer les URL dynamiques fournies par Render
+if [ -n "$ACCOUNT_SERVICE_HOST" ]; then
+    sed -i "s/__ACCOUNT_SERVICE_HOST__/${ACCOUNT_SERVICE_HOST}/g" /etc/nginx/nginx.conf
 fi
-
-# Remplacer le placeholder __RESOLVER__ par la vraie IP dans la configuration Nginx
-sed -i "s/__RESOLVER__/${RESOLVER}/g" /etc/nginx/nginx.conf
+if [ -n "$TRANSACTION_SERVICE_HOST" ]; then
+    sed -i "s/__TRANSACTION_SERVICE_HOST__/${TRANSACTION_SERVICE_HOST}/g" /etc/nginx/nginx.conf
+fi
 
 # Démarrer Nginx
 exec nginx -g 'daemon off;'
