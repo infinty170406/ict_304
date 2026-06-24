da4b2797a1093e19c9f768e5075bc3d70346e9cd
skyfall <skyfall@localhost.localdomain>
2026-06-24 01:18:57 +0100

    fix: use internal render network for proxy pass

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -11,13 +11,5 @@ fi
 # Remplacer le placeholder __RESOLVER__ par la vraie IP dans la configuration Nginx
 sed -i "s/__RESOLVER__/${RESOLVER}/g" /etc/nginx/nginx.conf
 
-# Remplacer les URL dynamiques fournies par Render
-if [ -n "$ACCOUNT_SERVICE_HOST" ]; then
-    sed -i "s/__ACCOUNT_SERVICE_HOST__/${ACCOUNT_SERVICE_HOST}/g" /etc/nginx/nginx.conf
-fi
-if [ -n "$TRANSACTION_SERVICE_HOST" ]; then
-    sed -i "s/__TRANSACTION_SERVICE_HOST__/${TRANSACTION_SERVICE_HOST}/g" /etc/nginx/nginx.conf
-fi
-
 # Démarrer Nginx
 exec nginx -g 'daemon off;'
