b75e059e67ce34a31fd6b0c69278cbed32545098
skyfall <skyfall@localhost.localdomain>
2026-06-24 01:34:44 +0100

    fix: revert to external urls to guarantee nginx startup

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# Fallbacks au cas où les variables d'environnement de Render soient vides
+ACCOUNT_HOST=${ACCOUNT_SERVICE_HOST:-"account-service-6db9.onrender.com"}
+TRANSACTION_HOST=${TRANSACTION_SERVICE_HOST:-"transaction-service-lyn1.onrender.com"}
+
+# Remplacer les URL dynamiques
+sed -i "s/__ACCOUNT_SERVICE_HOST__/${ACCOUNT_HOST}/g" /etc/nginx/nginx.conf
+sed -i "s/__TRANSACTION_SERVICE_HOST__/${TRANSACTION_HOST}/g" /etc/nginx/nginx.conf
+
 # Démarrer Nginx
 exec nginx -g 'daemon off;'
